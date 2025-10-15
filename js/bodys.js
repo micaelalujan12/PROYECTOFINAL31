cbde566894d74c2fd644acd15e4769e9ec09d07e
MicaUwU <micaelalujan387@gmail.com>
2025-10-15 14:16:45 -0300

    mica

---
+++ js/bodys.js
@@ -0,0 +1,18 @@
+
+function comprar() {
+
+    cantidad = cantidad + 1;
+    // Imprimimos la compra 
+    console.log(cantidad);
+    // Le avisamos la cantidad comprada 
+    alert("Compraste" + cantidad + "productos")
+}
+
+function carrito() {
+    totalPagar = cantidad * precio 
+
+    conosle.log (totalPagar);
+
+    resultado = pago - totalPagar
+}
+
